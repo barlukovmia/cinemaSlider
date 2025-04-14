3a97fdb6bd02cbd4724a22e4304a845dc3a2393a
Mikhail <barlukov.mia@phystech.edu>
2025-04-14 22:44:57 +0300

    update size for miniApp

--- cinemaSlider/frontend/src/App.js
+++ cinemaSlider/frontend/src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TinderSlider from './components/TinderSlider';
 
 function App() {
+    useEffect(() => {
+        if (window.Telegram && window.Telegram.WebApp) {
+            window.Telegram.WebApp.ready();
+            window.Telegram.WebApp.expand();
+    }
+  }, []);
     return (
         <div style={{overflow: "hidden"}} className="bg-gray-800 text-white min-h-screen flex items-center justify-center">
             <TinderSlider />
